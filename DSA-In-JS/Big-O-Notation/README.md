c8103934e607c7025f66bd29e072b48838d1d67c
Akash More <akashkmore1@gmail.com>
2022-05-19 00:25:48 +0530

    Learn Big O defination

--- DSA-In-JS/Big-O-Notation/README.md
+++ DSA-In-JS/Big-O-Notation/README.md
@@ -3,3 +3,41 @@
 1. When a problem has muliple solution, to find which solution is better, we use Big O notation.
 2. It's important to have a precise vocabulary to talk about how our code performs.
 3. When your code slows down or crashes, identifying parts of the code that are inefficient can help us find pain points in our applications.
+
+### What does a better code means?
+
+1. Faster
+2. Less memory intensive
+3. More Readable.
+
+### Why not to use timers?
+
+1. We used timers in SumOfNNumbers.js file. But generally we should not use timers.
+2. Different machines will record different times
+3. The same machine will record different times!
+4. For fast algorithms, speed measurements may not be precise enough?
+
+### If not time then what?
+
+1. Let's count the number of simple operations the computer has to perform!
+2. Time will always be determined by number of operations. They are directly proportional.
+
+## Introducing Big O
+
+1. Big O Notation is a way to formalize fuzzy counting. (Counting number of operations)
+2. It allows us to talk formally about how the runtime of an algorithm grows as the inputs grow.
+3. We won't care about the details, only the trends.
+
+### Big O defination:
+
+We say that an algorithm is O(f(n)) if the number of simple operations the computer has to do is eventually less than a constant times f(n), as n increases
+
+1. f(n) could be linear (f(n) = n)
+2. f(n) could be quadratic (f(n) = n )
+3. f(n) could be constant (f(n) = 1)
+4. f(n) could be something entirely different!
+
+### Big O of loop operations:
+
+1. For single loop: O(n)
+2. For loop inside loop: O(n^2)
--- DSA-In-JS/Big-O-Notation/SumOf-N-Number.js
+++ DSA-In-JS/Big-O-Notation/SumOf-N-Number.js
@@ -9,7 +9,14 @@ const sumOfNNumbersLoop = (n) => {
   return sum;
 };
 
-console.log(sumOfNNumbersLoop(1000000000));
+const t1Loop = performance.now();
+sumOfNNumbersLoop(1000000000);
+const t2Loop = performance.now();
+
+const timeByLoop = (t2Loop - t1Loop) / 1000;
+console.log("Time required for loop is ", timeByLoop);
+
+// ******************************************************************************************* //
 
 // 2nd approach
 const sumOfNNumbersFormula = (n) => {
@@ -17,7 +24,12 @@ const sumOfNNumbersFormula = (n) => {
   return sum;
 };
 
-console.log(sumOfNNumbersFormula(1000000000));
+const t1Formula = performance.now();
+sumOfNNumbersFormula(1000000000);
+const t2Formula = performance.now();
+
+const timeByFormula = (t2Formula - t1Formula) / 1000;
+console.log("Time required by formula is ", timeByFormula);
 
 // Which one of the above is better?
 
@@ -25,3 +37,18 @@ console.log(sumOfNNumbersFormula(1000000000));
 /*
 faster or less memory intesive or more readable
 */
+
+// To find which solution is faster we can use timer function
+// By timer function we can tell that loop approach is significantly slower than formula approach
+// time function is not ideal approach because:
+// Different machines will record different times
+// The same machine will record different times!
+// For fast algorithms, speed measurements may not be precise enough?
+
+// Instead we can count number of operations both approaches require:
+// First approach has 4 '=', 2 '+' operations.
+// But these operation are in loop so number of operation depend on n. We can say that 1st approach has n(4+6) operation.
+// Second approach has 4 operation, but they are not dependant on n. No matter what size of n is, this approach will have 4 operations only.
+
+// First operation has big O of n as it depends on n.
+// Second approach has Big O of 1 as it is constant time no matter what is value of n.
