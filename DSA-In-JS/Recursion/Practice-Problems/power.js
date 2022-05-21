bdd979d9ac7ffe23a7457537de4fe3cef0de8515
Akash More <akashkmore1@gmail.com>
2022-05-21 19:25:58 +0530

    Start recursion, solve power problem

---
+++ DSA-In-JS/Recursion/Practice-Problems/power.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("Hello World!");
+
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
+
+console.log(power(2, 3));
