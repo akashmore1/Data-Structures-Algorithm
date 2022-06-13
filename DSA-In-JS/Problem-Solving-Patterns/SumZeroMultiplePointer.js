1ec13c78c0dce308b1e9093abecd4df6b25f69c3
Akash More <akashkmore1@gmail.com>
2022-06-13 22:22:29 +0530

    Solve sum zero problem

--- DSA-In-JS/Problem-Solving-Patterns/SumZeroMultiplePointer.js
+++ DSA-In-JS/Problem-Solving-Patterns/SumZeroMultiplePointer.js
@@ -1,3 +1,20 @@
 "use strict";
 
 // Write a function called sumZero which accepts a sorted array of integers. The function should find the first pair where the sum is 0. Return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+const sumZero = (arr) => {
+  let start = 0;
+  let end = arr.length - 1;
+  while (arr[start] + arr[end] !== 0) {
+    if (arr[start] > arr[end]) {
+      return "No pair exist";
+    } else if (arr[start] + arr[end] > 0) {
+      end = end - 1;
+    } else if (arr[start] + arr[end] < 0) {
+      start = start + 1;
+    }
+  }
+  return `${arr[start]}, ${arr[end]}`;
+};
+
+console.log(sumZero([-4, -3, -2, -1, 1, 6]));
