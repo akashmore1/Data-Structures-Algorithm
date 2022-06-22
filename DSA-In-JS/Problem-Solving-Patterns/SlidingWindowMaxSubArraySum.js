e619436ba4a318bad8d441f8b543137fee97844c
Akash More <akashkmore1@gmail.com>
2022-06-22 09:11:18 +0530

    Solve sliding window maxSumSubArray problem

---
+++ DSA-In-JS/Problem-Solving-Patterns/SlidingWindowMaxSubArraySum.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// Write a function which takes an array of numbers and a number 'n' as input.
+// Function should calculate the maximum sum of 'n' consecutive elements in array.
+
+// Approach with complexity of O(n)  ==> Sliding Window
+const maxSubArraySum = (arr, num) => {
+  if (arr.length < num) {
+    return null;
+  }
+
+  let tempSum = 0;
+  for (let i = 0; i < num; i++) {
+    tempSum = tempSum + arr[i];
+  }
+  let maxSum = tempSum;
+
+  for (let i = 1; i < arr.length - num; i++) {
+    tempSum = tempSum - arr[i - 1] + arr[i + num - 1];
+    if (tempSum > maxSum) {
+      maxSum = tempSum;
+    }
+  }
+  return maxSum;
+};
+
+console.log(maxSubArraySum([1, 2, 3, 4, 6, 7, 8, 3, 2, 6, 4, 5, 1], 4));
