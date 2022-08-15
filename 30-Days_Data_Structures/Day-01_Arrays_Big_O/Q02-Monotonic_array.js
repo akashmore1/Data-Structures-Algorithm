95e0366d2bdbcd2f08af48fecb679ddd28cede1d
Akash More <akashkmore1@gmail.com>
2022-08-15 13:02:21 +0530

    Solve monotonic array problem in js

---
+++ 30-Days_Data_Structures/Day-01_Arrays_Big_O/Q02-Monotonic_array.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/*
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is monotone decreasing if for all i <= j, nums[i] >= nums[j].
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+*/
+
+const findMonotonicArray = (nums) => {
+  let index = 0;
+  let differenceValue = 0;
+  let tempDiff = 0;
+
+  while (differenceValue === 0 && index < nums.length - 1) {
+    differenceValue = nums[index] - nums[index + 1];
+    index++;
+  }
+  if (differenceValue < 0) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      tempDiff = nums[i] - nums[i + 1];
+      if (tempDiff > 0) {
+        return false;
+      }
+    }
+  } else if (differenceValue > 0) {
+    for (let i = 0; i < nums.length - 1; i++) {
+      tempDiff = nums[i] - nums[i + 1];
+      if (tempDiff < 0) {
+        return false;
+      }
+    }
+  } else {
+    return true;
+  }
+  return true;
+};
+
+console.log(
+  findMonotonicArray([11, 11, 9, 4, 3, 3, 3, 1, -1, -1, 3, 3, 3, 5, 5, 5]),
+  findMonotonicArray([11, 11, 9, 4, 3, 3, 3, 1, -1]),
+  findMonotonicArray([1, 2, 4, 6, 7, 88, 99]),
+  findMonotonicArray([4, 4, 4, 4, 4, 4, 4, 4, 4])
+);
