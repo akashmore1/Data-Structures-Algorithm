c7a6e9c5737cb4f18464525fba050c0b9f1ab028
Akash More <akashkmore1@gmail.com>
2022-08-15 21:52:54 +0530

    Add space O(1), time O(n) solution to rotate array

--- 30-Days_Data_Structures/Day-02_Arrays_Advanced/Q01-Rotate_array.js
+++ 30-Days_Data_Structures/Day-02_Arrays_Advanced/Q01-Rotate_array.js
@@ -33,3 +33,22 @@ var rotateRaw = function (nums, k) {
   }
   return nums;
 };
+// ======================================================================== //
+
+// ======================== space O(1) time (n) =========================== //
+const reverse = (arr, start, end) => {
+  while (start <= end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+  return arr;
+};
+
+const rotateArr = (nums, k) => {
+  k = k % nums.length;
+  nums = reverse(nums, 0, nums.length - 1);
+  nums = reverse(nums, 0, k - 1);
+  nums = reverse(nums, k, nums.length - 1);
+  return nums;
+};
