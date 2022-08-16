eb0c571587e5015a050f539c6caa46fbecb4c9f2
Akash More <akashkmore1@gmail.com>
2022-08-16 22:34:57 +0530

    Solve two sum with nlogn

--- 30-Days_Data_Structures/Day-03_Hash_Tables/Q01-Two_Sum_Problem.js
+++ 30-Days_Data_Structures/Day-03_Hash_Tables/Q01-Two_Sum_Problem.js
@@ -6,7 +6,7 @@ You can return the answer in any order.
 
 // =============================== Brut-force ================================= //
 
-const twoSum = (nums, target) => {
+const twoSumBrut = (nums, target) => {
   let answer;
   for (let i = 0; i < nums.length - 1; i++) {
     for (j = i + 1; j < nums.length; j++) {
@@ -21,6 +21,33 @@ const twoSum = (nums, target) => {
     return "No two values add upto target";
   }
 };
-console.log(twoSum([2, 7, 11, 15], 9));
+// console.log(twoSumBrut([2, 7, 11, 15], 9));
+
+// =============================================================================== //
+
+// ========================== nlogn time complexity ============================ //
+
+const twoSumWithSort = (nums, target) => {
+  const newArr = nums.slice(0).sort((a, b) => a - b);
+  let firstNumber, secondNumber;
+  let start = 0;
+  let end = newArr.length - 1;
+
+  while (start < end) {
+    // const sum = newArr[start] + newArr[end];
+    if (newArr[start] + newArr[end] > target) {
+      end--;
+    } else if (newArr[start] + newArr[end] < target) {
+      start++;
+    } else {
+      firstNumber = newArr[start];
+      secondNumber = newArr[end];
+      break;
+    }
+  }
+  const solArr = [nums.indexOf(firstNumber), nums.indexOf(secondNumber)];
+  return solArr;
+};
+console.log(twoSumWithSort([3, 3], 6)); // In this case this algorithm doesn't work
 
 // =============================================================================== //
