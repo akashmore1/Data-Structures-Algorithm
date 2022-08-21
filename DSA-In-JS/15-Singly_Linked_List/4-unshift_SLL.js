bed703625e9feab06b94ff1a5cef80702dbec23c
Akash More <akashkmore1@gmail.com>
2022-08-21 17:31:15 +0530

    Add unshift method to linked list

---
+++ DSA-In-JS/15-Singly_Linked_List/4-unshift_SLL.js
@@ -0,0 +1,82 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+    } else {
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+      this.length++;
+    }
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = this.head;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      this.length++;
+    }
+    return this;
+  }
+}
+
+const list = new SinglyLinkedList();
