5c036f69a9abeb75e19e1539707fb20af05bf067
Akash More <akashkmore1@gmail.com>
2022-08-21 16:20:20 +0530

    Learn shift method in linked list

---
+++ DSA-In-JS/15-Singly_Linked_List/3-Shift_SLL.js
@@ -0,0 +1,82 @@
+"use strict";
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+      this.length++;
+    } else {
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+      this.length++;
+    }
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = this.head;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  //   Let's work on shift method
+  //   shift() removed 1st element and returns it.
+  //   algo:
+  // If there are no node, return undefined
+  // Store current head property in a variable
+  // set the head property to current heads next property
+  // decrement length by 1
+  // return value of node removed
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+
+    let current = this.head;
+    this.head = current.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+      this.head = null;
+    }
+    return current;
+  }
+}
+
+const newList = new SinglyLinkedList();
+newList.push(1);
+newList.push(2);
+newList.push(3);
+newList.push(4);
