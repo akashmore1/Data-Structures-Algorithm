cb124e9511e5cb67a289ab617d2fdeb51511abdf
Akash More <akashkmore1@gmail.com>
2022-08-24 21:16:11 +0530

    Solve fibonacci with recursion

---
+++ 30-Days_Data_Structures/Day-04_Recursion/Q01-Fibonacci.js
@@ -0,0 +1,13 @@
+// In fibonaci
+// f(n) = f(n-1) + f(n-2)
+
+const fibonacci = (num) => {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
