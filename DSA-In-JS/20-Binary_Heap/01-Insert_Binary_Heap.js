e73dd37dc72ca512ff56c0c69aed727479fb49c2
Akash More <akashkmore1@gmail.com>
2022-08-26 21:49:21 +0530

    Binary tree initial setup

---
+++ DSA-In-JS/20-Binary_Heap/01-Insert_Binary_Heap.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Binary heap values are stored in an array
+// Binary heap array can be considered as breadth first search tree traversal
+
+// For any child at index 'n', its parent is at index (n-1)/2
+
+class BinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+}
