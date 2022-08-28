329e8d51c161a6b1c73d54dd6d7ab443e0339a5e
Akash More <akashkmore1@gmail.com>
2022-08-28 21:49:24 +0530

    Build priority queue

--- DSA-In-JS/20-Binary_Heap/04-Priority_Queue.js
+++ DSA-In-JS/20-Binary_Heap/04-Priority_Queue.js
@@ -6,13 +6,20 @@
 
 // First build min binary heap
 
-class MinBinaryHeap {
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  insert(val) {
-    this.values.push(val);
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
     return this.bubbleup();
   }
 
@@ -24,7 +31,7 @@ class MinBinaryHeap {
     while (index > 0) {
       let parentIndex = Math.floor((index - 1) / 2);
       let parent = this.values[parentIndex];
-      if (element > parent) break;
+      if (element.priority > parent.priority) break;
       this.values[parentIndex] = element;
       this.values[index] = parent;
 
@@ -34,7 +41,7 @@ class MinBinaryHeap {
     return this;
   }
 
-  extractMin() {
+  dequeue() {
     const min = this.values[0];
     if (this.values.length > 1) {
       const element = this.values.pop();
@@ -58,14 +65,17 @@ class MinBinaryHeap {
 
       if (leftChildIndex < length) {
         leftChild = this.values[leftChildIndex];
-        if (leftChild < element) {
+        if (leftChild.priority < element.priority) {
           swap = leftChildIndex;
         }
       }
 
       if (rightChildIndex < length) {
         rightChild = this.values[rightChildIndex];
-        if (rightChild < element && rightChild < leftChild) {
+        if (
+          rightChild.priority < element.priority &&
+          rightChild.priority < leftChild.priority
+        ) {
           swap = rightChildIndex;
         }
       }
@@ -80,15 +90,15 @@ class MinBinaryHeap {
   }
 }
 
-const heap = new MinBinaryHeap();
-heap.insert(1);
-heap.insert(2);
-heap.insert(4);
-heap.insert(71);
-heap.insert(22);
-heap.insert(111);
-heap.insert(21);
-heap.insert(44);
-heap.insert(72);
-heap.insert(34);
-heap.insert(83);
+const heap = new PriorityQueue();
+heap.enqueue("abcs", 2);
+heap.enqueue("abcs", 1);
+heap.enqueue("abcs", 4);
+heap.enqueue("abcs", 71);
+heap.enqueue("abcs", 22);
+heap.enqueue("abcs", 111);
+heap.enqueue("abcs", 21);
+heap.enqueue("abcs", 44);
+heap.enqueue("abcs", 72);
+heap.enqueue("abcs", 34);
+heap.enqueue("abcs", 83);
