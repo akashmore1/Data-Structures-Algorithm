ce5a518ba635dc389fce3e5ee3f601ea963377fd
Akash More <akashkmore1@gmail.com>
2022-08-31 18:52:17 +0530

    Add edge in graph

---
+++ DSA-In-JS/22-Graphs/03-Add_Edges.js
@@ -0,0 +1,21 @@
+"use strict";
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+}
+
+const g = new Graph();
+g.addVertex("India");
+g.addVertex("France");
+g.addEdge("India", "France");
