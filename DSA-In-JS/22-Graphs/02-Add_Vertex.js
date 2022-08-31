9c6168cebe280e21eba845977fd1edfc1d069aec
Akash More <akashkmore1@gmail.com>
2022-08-31 18:46:45 +0530

    Add vertex in a graph

---
+++ DSA-In-JS/22-Graphs/02-Add_Vertex.js
@@ -0,0 +1,15 @@
+"use strict";
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+}
+
+const g = new Graph();
+g.addVertex("India");
+g.addVertex("France");
