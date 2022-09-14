3e67e8ab85a9a27765216b54299b73ce66927ba3
Akash More <akashkmore1@gmail.com>
2022-09-14 19:37:46 +0530

    Learn about the power set

---
+++ 30-Days_Data_Structures/Day-05_Recursion_Advanced/Q02-Powerset_Itr.js
@@ -0,0 +1,3 @@
+// A power set is a set which contains all subsets.
+// Power set of {1,2,3} = {{}, {1}, {2}, {1, 2}, {3}, {1, 3}, {2, 3}, {1, 2, 3}}
+// For n elements = 2^n subsets i.e. powerSet.size = 2^n
