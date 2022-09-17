c4d9102160e991c913e0575ee6d6124d29c1526c
Akash More <akashkmore1@gmail.com>
2022-09-17 20:33:05 +0530

    IsPalindrom 2 pointer

---
+++ 30-Days_Data_Structures/Day-06_Strings/Q02-isPalindrome_Two_Pointer.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const isPalindrome = (str) => {
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start <= end) {
+    if (str[start] !== str[end]) return false;
+    start++;
+    end--;
+  }
+
+  return true;
+};
+
+console.log(isPalindrome("malayalam"));
