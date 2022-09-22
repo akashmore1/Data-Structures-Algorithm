98a555fd063c4344d0e9e44d210c3b8df20dddbf
Akash More <akashkmore1@gmail.com>
2022-09-22 22:08:49 +0530

    optimized solution for group anagrams

--- 30-Days_Data_Structures/Day-07_Strings_II/Q02-Group_Anagram.js
+++ 30-Days_Data_Structures/Day-07_Strings_II/Q02-Group_Anagram.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+// typically using all the original letters exactly once.
+
+// 👇Below is going to be Brute-Force method, solve it later:
 const checkIfAnagram = (str1, str2) => {
   if (str1.length !== str2.length) return false;
   if (str1 === "") return true;
@@ -35,4 +40,36 @@ const checkIfAnagram = (str1, str2) => {
   }
 
   return true;
+};
+
+const groupAnagramsBrut = (arr) => {};
+
+groupAnagramsBrut(["cat", "act", "tap"]);
+
+// ========================================================================================================== //
+
+// Best solution using sort:
+
+const groupAnagrams = (strs) => {
+  const anagramObj = {};
+  const result = [];
+
+  const sortedStrs = strs.map((e) => {
+    return [...e].sort().join("");
+  });
+
+  for (let i = 0; i < sortedStrs.length; i++) {
+    const str = sortedStrs[i];
+    if (str in anagramObj) {
+      anagramObj[str].push(strs[i]);
+    } else {
+      anagramObj[str] = [strs[i]];
+    }
+  }
+
+  for (let key in anagramObj) {
+    result.push(anagramObj[key]);
+  }
+
+  return result;
 };
