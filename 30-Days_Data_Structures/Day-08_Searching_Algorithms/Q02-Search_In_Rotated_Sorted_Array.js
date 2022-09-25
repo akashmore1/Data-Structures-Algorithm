40497f0d5dab4178c199b70b026429ec8cae3ab1
Akash More <akashkmore1@gmail.com>
2022-09-25 18:36:09 +0530

    Search in sorted rotated array solution

--- 30-Days_Data_Structures/Day-08_Searching_Algorithms/Q02-Search_In_Rotated_Sorted_Array.js
+++ 30-Days_Data_Structures/Day-08_Searching_Algorithms/Q02-Search_In_Rotated_Sorted_Array.js
@@ -5,38 +5,38 @@
 // Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
 // You must write an algorithm with O(log n) runtime complexity.
 
-// const search = (nums, target) => {
-//   let left = 0;
-//   let right = nums.length - 1;
-//   let middle;
+const search = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let middle;
 
-//   while (left <= right) {
-//     middle = Math.floor((left + right) / 2);
+  while (left <= right) {
+    middle = Math.floor((left + right) / 2);
 
-//     if (nums[middle] === target) return middle;
+    if (nums[middle] === target) return middle;
 
-//     if (nums[left] <= nums[middle]) {
-//       // sorted
-//       if (target >= nums[left] && target < nums[middle]) {
-//         // explore left part
-//         right = middle - 1;
-//       } else {
-//         // explore right part
-//         left = middle + 1;
-//       }
-//     } else {
-//       // sorted
-//       if (target > nums[middle] && target <= nums[right]) {
-//         // explore right part
-//         left = middle + 1;
-//       } else {
-//         // explore left part
-//         right = middle - 1;
-//       }
-//     }
-//   }
+    if (nums[left] <= nums[middle]) {
+      // sorted
+      if (target >= nums[left] && target < nums[middle]) {
+        // explore left part
+        right = middle - 1;
+      } else {
+        // explore right part
+        left = middle + 1;
+      }
+    } else {
+      // sorted
+      if (target > nums[middle] && target <= nums[right]) {
+        // explore right part
+        left = middle + 1;
+      } else {
+        // explore left part
+        right = middle - 1;
+      }
+    }
+  }
 
-//   return -1;
-// };
+  return -1;
+};
 
-// console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
