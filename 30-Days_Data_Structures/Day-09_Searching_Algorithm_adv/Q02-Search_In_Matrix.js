6768e844d7abfd0747eba421fd50adfd79e4a14e
Akash More <akashkmore1@gmail.com>
2022-10-06 22:18:52 +0530

    Binary search in matrix approach

---
+++ 30-Days_Data_Structures/Day-09_Searching_Algorithm_adv/Q02-Search_In_Matrix.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+
+// First find relevent row in which number can be present
+// second apply binary search on that row
+const searchInMatrix = () => {};
