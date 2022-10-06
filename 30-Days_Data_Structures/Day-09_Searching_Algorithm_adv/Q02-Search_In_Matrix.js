8698ec97cef99ccdc623d9b1e09cc8c96b35f59b
Akash More <akashkmore1@gmail.com>
2022-10-06 22:37:18 +0530

    Binary search in matrix

--- 30-Days_Data_Structures/Day-09_Searching_Algorithm_adv/Q02-Search_In_Matrix.js
+++ 30-Days_Data_Structures/Day-09_Searching_Algorithm_adv/Q02-Search_In_Matrix.js
@@ -6,4 +6,69 @@
 
 // First find relevent row in which number can be present
 // second apply binary search on that row
-const searchInMatrix = () => {};
+
+// A matrix is nothing but 2D array
+
+// matrix = [
+//  [1, 2, 3],
+//  [4, 6, 7]
+//  [9, 11, 14]
+// ]
+var searchMatrix = function (matrix, target) {
+  const relevantRow = findRelevantRow(matrix, target);
+
+  if (relevantRow === -1) {
+    return false;
+  }
+
+  const arrayToSearch = matrix[relevantRow];
+
+  let start = 0;
+  let end = arrayToSearch.length - 1;
+  let middle;
+
+  while (start <= end) {
+    middle = Math.floor((start + end) / 2);
+
+    if (arrayToSearch[middle] === target) {
+      return true;
+    }
+
+    if (target < arrayToSearch[middle]) {
+      end = middle - 1;
+    }
+
+    if (target > arrayToSearch[middle]) {
+      start = start + 1;
+    }
+  }
+
+  return false;
+};
+
+const findRelevantRow = (matrix, target) => {
+  let start = 0;
+  let end = matrix.length - 1;
+  let middle;
+
+  while (start <= end) {
+    middle = Math.floor((start + end) / 2);
+
+    if (
+      matrix[middle][0] <= target &&
+      matrix[middle][matrix[middle].length - 1] >= target
+    ) {
+      return middle;
+    }
+
+    if (matrix[middle][0] > target) {
+      end = middle - 1;
+    }
+
+    if (matrix[middle][matrix[middle].length - 1] < target) {
+      start = middle + 1;
+    }
+  }
+
+  return -1;
+};
