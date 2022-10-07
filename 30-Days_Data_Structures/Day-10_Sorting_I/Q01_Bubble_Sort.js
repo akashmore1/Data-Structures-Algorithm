617a8c731ac31234e352b80ae65aa53f7c21bc60
Akash More <akashkmore1@gmail.com>
2022-10-07 22:28:41 +0530

    Bubble sort

---
+++ 30-Days_Data_Structures/Day-10_Sorting_I/Q01_Bubble_Sort.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log("Hello World");
+const swap = (arr, i1, i2) => {
+  [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
+};
+
+const bubbleSort = (arr) => {
+  let isSwapping = false;
+  for (let j = arr.length; j > 0; j--) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr, i, i + 1);
+        isSwapping = true;
+      }
+    }
+    if (!isSwapping) {
+      break;
+    }
+  }
+  return arr;
+};
+
+console.log(bubbleSort([4, 1, 2, 3, 6, 7, 8]));
