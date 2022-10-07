e1687ee53211d9a569710b3ed3c5f931bb90cc46
Akash More <akashkmore1@gmail.com>
2022-10-07 22:46:12 +0530

    Insertion sort algorithm

--- 30-Days_Data_Structures/Day-10_Sorting_I/Q02_Insertion_Sort.js
+++ 30-Days_Data_Structures/Day-10_Sorting_I/Q02_Insertion_Sort.js
@@ -1,5 +1,10 @@
 "use strict";
 
+// Consider 1st element as sorted part of array
+// Now add 2nd element to this sorted part of array
+// i.e. if 2nd element is smaller than first then swap() else do nothing
+// Repeat this process i.e. Increase size of sorted part of array by adding one by one elements
+
 const swap = (arr, i1, i2) => {
   [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
 };
