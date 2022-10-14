4028fd93abc76a5d329a5a6aeec799d06a52df29
Akash More <akashkmore1@gmail.com>
2022-10-14 23:31:15 +0530

    Implement selection sort

--- DSA-In-JS/10-Selection_Sort/Selection_Sort.js
+++ DSA-In-JS/10-Selection_Sort/Selection_Sort.js
@@ -5,26 +5,25 @@ In this sort, we find the smallest value then place it at first.
 then again repeat this process until every element is place at right place
 */
 
-// const swap = (arr, i1, i2) => {
-//   [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
-// };
+const swap = (arr, i1, i2) => {
+  console.log(arr[i1], arr[i2]);
+  [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
+};
 
-// const selectionSort = (arr) => {
-//   let min = Infinity;
-//   let minIndex;
+const selectionSort = (arr) => {
+  for (let j = 0; j < arr.length; j++) {
+    let minIndex = j;
+    for (let i = j + 1; i < arr.length; i++) {
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
 
-//   for (let j = 0; j < arr.length; j++) {
-//     for (let i = j; i < arr.length; i++) {
-//       if (arr[i] < min) {
-//         minIndex = i;
-//       }
+      if (i === arr.length - 1) {
+        swap(arr, minIndex, j);
+      }
+    }
+  }
+  return arr;
+};
 
-//       if (i === arr.length - 1) {
-//         swap(arr, minIndex, j);
-//       }
-//     }
-//   }
-//   return arr;
-// };
-
-// console.log(selectionSort([4, 3, 2, 1]));
+console.log(selectionSort([4, 3, 2, 1]));
