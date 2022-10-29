1634b5dffdeab6da35b95a3fecc09bbdd1f17324
Akash More <akashkmore1@gmail.com>
2022-10-29 22:08:42 +0530

    Pseudocode for merge sort

---
+++ 30-Days_Data_Structures/Day-11_Sorting_II/Q02_Merge_Sort.js
@@ -0,0 +1,5 @@
+"use strict";
+
+// First Write algorithm for merging to sorted arrays
+// Then divide given array to smaller array until its 1 or 0 element array, then these smaller arrays are sorted arrays
+// Merge these arrays to eventually merge entire array
